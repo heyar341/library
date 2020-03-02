6d7ff5d3ef15fdc49c4a842db9f994485223c299
heyar341 <kityonya@gmail.com>
2020-03-02 17:38:27 +0900

    Testに沿って作成

--- app/Http/Controllers/BooksController.php
+++ app/Http/Controllers/BooksController.php
@@ -9,20 +9,32 @@ class BooksController extends Controller
 {
     public function store(){
 
-        $data = $this->validateRequest();
-        Book::create($this->validateRequest());
+        $book = Book::create($this->validateRequest());
+        return redirect($book->path());
     }
 
     public function update(Book $book)
     {
         $data = $this->validateRequest();
         $book->update($this->validateRequest());
+
+        return redirect($book->path());
     }
+
+
+    public function destroy(Book $book)
+    {
+        $book->delete();
+
+        return redirect('/books');
+    }
+
 
     protected function validateRequest(){
         return request()->validate([
             'title' => 'required',
             'author' => 'required',
         ]);
-}
+    }
+
 }
