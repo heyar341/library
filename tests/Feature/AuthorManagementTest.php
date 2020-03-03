9f57720e2757c4749767c96efcdddb13d18631c1
heyar341 <kityonya@gmail.com>
2020-03-03 10:06:02 +0900

    Authorのテスト修正

--- tests/Feature/AuthorManagementTest.php
+++ tests/Feature/AuthorManagementTest.php
@@ -15,7 +15,6 @@ class AuthorManagementTest extends TestCase
 
     public function an_author_can_be_created()
     {
-        $this->withoutExceptionHandling();
 
         $this->post('/author',[
             'name' => 'Author Name',
